5a0f6a88b042ac663c825534c293371adb3581dc
Aaron Heiner <aheiner2001@gmail.com>
2026-02-06 18:29:17 -0700

    test

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import csv
-with open('pizza.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row['date'])
+with open('pizza.csv','w', newline='') as csvfile:
 
+    field_names = ['date','time_in_fridge_days','rest_time_before_fridge_hours','yeast_amount_tsp','bake_day_sit_time_hours','bake_day_sit_method','bake_day_reball','ball_folds','ball_tightness','notes','result_texture','result_chewiness','result_oven_spring','result_overall']
+    writer = csv.DictWriter(csvfile, fieldnames=field_names)
+
+    writer.writeheader()
+    row_data = {}
+    for field in field_names:
+        row_data[field] = input(f"Enter {field}: ")
+    writer.writerow(row_data)
 
 
         # this is from the git branch test
-        print("this is a test branch using git")
-print(row)
+        
