23937081aa7fc17ec5347c51559a7f4035588f3a
Aaron Heiner <aheiner2001@gmail.com>
2026-02-06 18:35:22 -0700

    tets8ing

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import csv
-with open('pizza.csv','w', newline='') as csvfile:
+with open('pizza.csv','a', newline='') as csvfile:
 
     field_names = [ 'yeast_amount_tsp', 'rest_time_before_fridge_hours','ball_tightness', 'time_in_fridge_days', 'bake_day_sit_time_hours', 'bake_day_sit_method', 'ball_folds',  'bake_day_reball', 'result_texture', 'result_chewiness', 'result_oven_spring', 'result_overall', 'notes']
     writer = csv.DictWriter(csvfile, fieldnames=field_names)
-
-    writer.writeheader()
     row_data = {}
     for field in field_names:
         row_data[field] = input(f"Enter {field}: ")
